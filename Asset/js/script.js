17459b6922ccd7a670ab89b7567e7f6ac3049e7a
Sheik Dawood <sheikdawoodaym1@gmail.com>
2025-10-23 19:20:43 +0530

    commit

---
+++ Asset/js/script.js
@@ -0,0 +1,75 @@
+// // scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('.navbar-section .navbar .navbar-items .nav-list .nav-items a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 550;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        // highlight nav link
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document
+                    .querySelector('.navbar-section .navbar .navbar-items .nav-list .nav-items a[href*=' + id + ']')
+                    .classList.add('active');
+            });
+
+            // Add class for animation
+            sec.classList.add('show-animate');
+        } 
+        // else {
+        //     sec.classList.remove('show-animate');
+        // }
+    });
+
+    // Sticky navbar
+    let header = document.querySelector(".navbar-section");
+    header.classList.toggle('sticky', window.scrollY > 100);
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+
+// Projects Carousel
+const projects = [
+    { title: "Logico", image: "./Asset/img/Project-1.jpeg", link: "https://example.com/electro" },
+    { title: "Talented Men's Barber Studio", image: "./Asset/img/Project-2.jpeg", link: "https://sheikdawood2003.github.io/talentedmensbarber-studio/" },
+    { title: "Logistic", image: "./Asset/img/Project-3.jpeg", link: "https://sheikdawood2003.github.io/logistic/" },
+    { title: "Upsoft", image: "./Asset/img/Project-4.jpeg", link: "https://sheikdawood2003.github.io/Upsoft/" },
+    { title: "College Website", image: "./Asset/img/Project-5.jpeg", link: "https://sheikdawood2003.github.io/collegesite/" },
+    { title: "Bus Booking App", image: "./Asset/img/Project-6.jpeg", link: "https://sheikdawood2003.github.io/Busbookingapp/" }
+  ];
+
+  let currentIndex = 0;
+
+  function openProject(index) {
+    currentIndex = index;
+    document.getElementById("projectModal").classList.add("active");
+    updateModal();
+  }
+
+  function closeProject() {
+    document.getElementById("projectModal").classList.remove("active");
+  }
+
+  function updateModal() {
+    const project = projects[currentIndex];
+    document.getElementById("projectTitle").textContent = project.title;
+    document.getElementById("projectImage").src = project.image;
+    document.getElementById("projectLink").href = project.link;
+  }
+
+  function nextProject() {
+    currentIndex = (currentIndex + 1) % projects.length;
+    updateModal();
+  }
+
+  function prevProject() {
+    currentIndex = (currentIndex - 1 + projects.length) % projects.length;
+    updateModal();
+  }
